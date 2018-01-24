2fbb21fe57681a28954a75a65bce492e26b2b741
Jaakko Ojalehto <jaakko.ojalehto@yle.fi>
2018-01-24 20:57:28 +0200

    Initial commit

---
+++ js/template.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill'
+import 'raf/polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <h1>Hello from React!</h1>
+  , document.getElementById('template'),
+)
